82a184233f4f6e1ad15c90a2ae60a770e1f8e0ef
Matthew <msb2170@gmail.com>
2023-06-22 12:00:19 -0400

    near final only need to grab email for success page, optimize for mobile, and make minor style changes

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const button = document.getElementById('button')
+const dismiss = document.getElementById('dismiss')
+const mainDiv = document.getElementById('container')
+const successDiv = document.getElementById('success-container')
 
-const handleClick = () => {
-    console.log('clicked')
-}
+button.addEventListener('click', () => {
+    mainDiv.style.display = 'none';
+    successDiv.style.display = 'flex';
+})
+
+dismiss.addEventListener('click', () => {
+    mainDiv.style.display = 'flex';
+    successDiv.style.display = 'none';
+})
+
