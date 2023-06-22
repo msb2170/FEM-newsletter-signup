ca86bdf2eaa0976d39833d9f53f4e13b254180ab
Matthew <msb2170@gmail.com>
2023-06-22 14:00:23 -0400

    feature complete, mobile styling in progress

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const button = document.getElementById('button')
 const dismiss = document.getElementById('dismiss')
 const mainDiv = document.getElementById('container')
 const successDiv = document.getElementById('success-container')
+const inputText = document.getElementById('email')
+const textDisplay = document.getElementById('email-input')
 
 button.addEventListener('click', () => {
     mainDiv.style.display = 'none';
     successDiv.style.display = 'flex';
+    textDisplay.innerHTML = inputText.value;
 })
 
 dismiss.addEventListener('click', () => {
     mainDiv.style.display = 'flex';
     successDiv.style.display = 'none';
 })
+
 
