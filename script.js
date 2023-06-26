4fe75fa81ed50b87c3c2594565cd22bc2815e257
Matthew <msb2170@gmail.com>
2023-06-26 14:37:30 -0400

    final

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ const mainDiv = document.getElementById('container')
 const successDiv = document.getElementById('success-container')
 const inputText = document.getElementById('email')
 const textDisplay = document.getElementById('email-input')
+const warnText = document.getElementById('warn-text')
+
+function isValidEmail(email) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email)
+}
 
 button.addEventListener('click', () => {
-    mainDiv.style.display = 'none';
-    successDiv.style.display = 'flex';
-    textDisplay.innerHTML = inputText.value;
+    if (isValidEmail(inputText.value)) {
+        console.log('valid')
+        mainDiv.style.display = 'none';
+        successDiv.style.display = 'flex';
+        textDisplay.innerHTML = inputText.value;
+    } else {
+        warnText.style.display = 'inline-block';
+    }
+    
 })
 
 dismiss.addEventListener('click', () => {
